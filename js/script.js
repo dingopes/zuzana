b58d999ce07c00973951a5f759f6463401955cb5
D D <D D>
2016-10-21 15:29:12 +0200

    1

---
+++ js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+
+$("#toggleMenu").click(function() {
+    $(".row ul").toggle('slide', {direction: 'left'}, 500);
+});
+
+$("#menuOut").click(function() {
+    $(".row ul").toggle('slide', {direction: 'left'}, 500);
+});
+});
